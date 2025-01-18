be9ec2d46bb4fb932ee21c21b756ed037ec34bf2
Adrian <72287783+adriannbhp@users.noreply.github.com>
2025-01-18 20:44:23 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://front-end-proyek-3.vercel.app/',],
+  origin: ['https://front-end-proyek-3.vercel.app',],
   credentials: true
 }));
 
